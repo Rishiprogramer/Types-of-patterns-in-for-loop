e1824f7c39bd21ea15b77a2b04ef409acfdc7b34
Rishi Prasad Narayan Singh <122547010+Rishiprogramer@users.noreply.github.com>
2023-07-23 19:54:00 +0530

    Create nice_paterns.py

---
+++ nice_paterns.py
@@ -0,0 +1,9 @@
+for i in range(2, 12, 2):
+    for j in range(2,14-i, 2):
+        print(j, end=" ")
+    print()
+    
+for i in range(2, 12, 2):
+    for j in range(2, i+1 ,2):
+        print(j, end=" ")
+    print()
